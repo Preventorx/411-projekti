603be4dac0e538223252656f68b08808ae82afe4
Preventorx <preventorx@gmail.com>
2025-06-27 17:26:59 +0300

    Lisätty tyylit, aloitettu javascriptiä

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function laheta() {
-    
+function validointi() {
+    //resetoidaan error
+    const errorElements = document.querySelectorAll(".error");
+    errorElements.forEach(element => {
+        element.textContent = "";
+    })
+
+    const kayttaja = document.getElementById("kayttaja").value;
+    const salasana = document.getElementById("salasana").value;
+    const nimi = document.getElementById("nimi").value;
+    const osoite = document.getElementById("osoite").value;
+    const maa = document.getElementById("maa").value;
+    const postinumero = document.getElementById("postinumero").value;
+    const sposti = document.getElementById("sposti").value;
+    const sukupuoli = document.querySelector("input[name='sukupuoli']:checked");
+    const kieli = document.querySelector("input[name='kieli']:checked");
+
+    //validointi 
+    let isValid = true
+
+
+
+
 }
